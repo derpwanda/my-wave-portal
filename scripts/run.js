075b99fc22f53ea4c1b07fd3d6bbafe05b1cc1fd
Wanda Evans <derpwanda@gmail.com>
2021-12-18 12:58:12 -0600

    store our messages in an array using structs

--- scripts/run.js
+++ scripts/run.js
@@ -1,7 +1,7 @@
 const main = async () => {
 
-    const waveMap = new Map()
-    const [owner, randomPerson] = await hre.ethers.getSigners();
+    const [owner, anotherPerson] = await hre.ethers.getSigners();
+
     const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
     const waveContract = await waveContractFactory.deploy();
     await waveContract.deployed();
@@ -11,21 +11,18 @@ const main = async () => {
 
     let waveCount;
     waveCount = await waveContract.getTotalWaves();
+    console.log(waveCount.toNumber());
 
-    let waveTxn = await waveContract.wave();
+    let waveTxn = await waveContract.wave('Hello there message!');
     await waveTxn.wait();
 
-    waveCount = await waveContract.getTotalWaves();
+    const [_, randomPerson] = await hre.ethers.getSigners();
+    waveTxn = await waveContract.connect(randomPerson).wave('Another message!');
+    await waveTxn.wait(); // Wait for the transaction to be mined
 
-    const createMap = () => {
-        if (owner.address !== waveMap) {
-            waveMap.set(owner.address, waveCount)
-        } else {
-            waveMap.get(owner.address)++
-        }
-        console.log(waveMap)
-    }
-    await createMap();
+    let allWaves = await waveContract.getAllWaves();
+    console.log(allWaves);
+
 };
 
 const runMain = async () => {
