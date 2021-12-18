652a6338b2f435542fc186757acb33cea3790367
Wanda Evans <derpwanda@gmail.com>
2021-12-18 13:30:05 -0600

    updated wave func to send eth

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
-
 contract WavePortal {
     uint256 totalWaves;
 
@@ -29,6 +28,14 @@ contract WavePortal {
         waves.push(Wave(msg.sender, _message, block.timestamp));
 
         emit NewWave(msg.sender, block.timestamp, _message);
+
+        uint256 prizeAmount = 0.0001 ether;
+        require(
+            prizeAmount <= address(this).balance,
+            "Trying to withdraw more money than the contract has."
+        );
+        (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+        require(success, "Failed to withdraw money from contract.");
     }
 
     // added function that returns the struct to us. how we retrieve waves.timestamp
