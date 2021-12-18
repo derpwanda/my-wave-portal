025a6358995f096d97410aec34e5ebebbb72282a
Wanda Evans <derpwanda@gmail.com>
2021-12-18 14:14:31 -0600

    added 15m cooldown

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -18,6 +18,12 @@ contract WavePortal {
 
     Wave[] waves;
 
+    /*
+     * This is an address => uint mapping, meaning I can associate an address with a number!
+     * In this case, I'll be storing the address with the last time the user waved at us.
+     */
+    mapping(address => uint256) public lastWavedAt;    
+
     constructor() payable {
         //now we are allowed to pay people
         console.log("Smart Contract constructed");
@@ -26,6 +32,19 @@ contract WavePortal {
     }
 
     function wave(string memory _message) public {
+        /*
+         * We need to make sure the current timestamp is at least 15-minutes bigger than the last timestamp we stored
+         */
+        require(
+            lastWavedAt[msg.sender] + 15 minutes < block.timestamp,
+            "Wait 15m"
+        );
+
+        /*
+         * Update the current timestamp we have for the user
+         */
+        lastWavedAt[msg.sender] = block.timestamp;
+        
         totalWaves += 1;
         console.log("%s has waved w message %s!", msg.sender, _message);
 
